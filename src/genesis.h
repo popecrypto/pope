b46e6783ea26b37c511364626d2bdce2f5b39b30
mail <you@example.com>
2018-05-24 21:46:26 +0200

    first commit

---
+++ src/genesis.h
@@ -0,0 +1,12 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x000006009742db6089cde9ae8c5b697a54d3b24f46fb4269543c9fd005662c87");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+#endif // GENESIS_H
